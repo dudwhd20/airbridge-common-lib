eb628d7929e38622215247e995d1316dcb1a0686
soyj <dudwhd20@naver.com>
2025-08-06 10:52:13 +0900

    chore: add mavenLocal publish & lib upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,19 @@ plugins {
     kotlin("jvm") version "2.2.0"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.4"
+    `maven-publish`
 }
 
 group = "com.airbridge.common"
 version = "1.0-SNAPSHOT"
+
+publishing{
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -18,7 +27,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
     testImplementation(kotlin("test"))
 }
 
